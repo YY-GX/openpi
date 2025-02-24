b556f4f654195b147285b2ac762424031f85c95a
yy-gx <yyang9923@gmail.com>
2025-02-24 13:50:50 -0500

    update configs

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -520,7 +520,8 @@ _CONFIGS = [
     ),
     TrainConfig(
         name="pi0_fast_libero_low_mem_finetune_yy",
-        model=pi0_fast.Pi0FASTConfig(paligemma_variant="gemma_2b_lora"),
+        # model=pi0_fast.Pi0FASTConfig(paligemma_variant="gemma_2b_lora"),
+        model=pi0_fast.Pi0FASTConfig(action_dim=7, action_horizon=10, max_token_len=180, paligemma_variant="gemma_2b_lora"),
         data=LeRobotLiberoDataConfig(
             repo_id="yygx/libero90",
             base_config=DataConfig(
